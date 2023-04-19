df799a868c02f6cb4da12c37a6189eb0c956acfc
Anton <hulkaach@mail.ru>
2023-04-19 19:39:10 +0300

    commit 2

--- src/main/java/HomeWork/Ex2/Ex2.java
+++ src/main/java/HomeWork/Ex2/Ex2.java
@@ -2,6 +2,24 @@ package HomeWork.Ex2;
 
 public class Ex2 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "the sky is blue";
+        char[] ass = str.toCharArray();
+        int i = 0;
+        int j = ass.length-1;
+
+        while (i <= j) {
+            char temp = ass[i];
+            ass[i] = ass[j];
+            ass[j] = temp;
+            i++;
+            j--;
+
+        }
+        System.out.println(ass);
+
+
+
+
     }
+
 }
